dbb0390626566d713a3db00ef69569fea7bcb7c3
gameryahiam <yahiamahmoud357@gmail.com>
2026-08-05 19:01:08 +0300

    add README and code files for lesson 05 and lesson 06

---
+++ lesson-05/code.py
@@ -0,0 +1,17 @@
+#1+1=3           #error
+print("i am programmer")
+
+#print("i am programmer")print("i am programmer")       #syntax error
+print("i am programmer");print("i am programmer")       #this is correct
+
+
+#Print("i am programmer")       #error
+
+#print(true)       #error
+
+print(True)        #this is correct
+
+print(1)
+print(2)
+print(4)
+print(3)    
---
+++ lesson-06/code.py
@@ -0,0 +1,4 @@
+name = "Alice"
+print(name)
+print("Hello, " + name + "!")   
+    
