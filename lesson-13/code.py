5ac3a13ffa79cebeb60aac3e5ce44af2cbd80e66
gameryahiam <yahiamahmoud357@gmail.com>
2026-08-09 16:19:43 +0300

    add README and code files for lesson 14

--- lesson-13/code.py
+++ lesson-13/code.py
@@ -7,3 +7,17 @@ print(members_party2.split("_"))
 members_party3 = ("Alice_Bob_Charlie_David_Eve_Frank_Grace_Heidi_Ivan_Judy")
 print(members_party3.split("a"))
 
+members_party4 = ("Alice_Bob_Charlie_David_Eve_Frank_Grace_Heidi_Ivan_Judy")
+print(members_party4.split("a"))
+
+members_party5 = ("Alice Bob Charlie David Eve Frank Grace Heidi Ivan Judy")
+print(members_party5.rsplit())
+
+members_party6 = ("Alice_Bob_Charlie_David_Eve_Frank_Grace_Heidi_Ivan_Judy")
+print(members_party6.rsplit("_"))
+
+members_party7 = ("Alice_Bob_Charlie_David_Eve_Frank_Grace_Heidi_Ivan_Judy")
+print(members_party7.rsplit("a"))
+
+members_party8 = ("Alice_Bob_Charlie_David_Eve_Frank_Grace_Heidi_Ivan_Judy")
+print(members_party8.rsplit("a",3))
---
+++ lesson-14/code.py
@@ -0,0 +1,5 @@
+x = "yahia mahmoud mahamad hassan athman ahmed mohamed abd-afar"
+print(x.index("ahmed"))
+print(len(x))
+print(x.replace("yahia", "zakaria"))
+
