21f732544d3849a11afda99ffd094d5088a3bfc9
gameryahiam <yahiamahmoud357@gmail.com>
2026-08-09 14:56:32 +0300

    add README and code files for lesson 13

---
+++ lesson-13/code.py
@@ -0,0 +1,9 @@
+members_party = ("Alice Bob Charlie David Eve Frank Grace Heidi Ivan Judy")
+print(members_party.split())
+
+members_party2 = ("Alice_Bob_Charlie_David_Eve_Frank_Grace_Heidi_Ivan_Judy")
+print(members_party2.split("_"))
+
+members_party3 = ("Alice_Bob_Charlie_David_Eve_Frank_Grace_Heidi_Ivan_Judy")
+print(members_party3.split("a"))
+
