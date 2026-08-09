8959f586f1c31b97d8a7e3d90751932fc6fde322
gameryahiam <yahiamahmoud357@gmail.com>
2026-08-09 14:27:30 +0300

    add README and code files for lesson 11 and lesson 12

---
+++ lesson-12/code.py
@@ -0,0 +1,11 @@
+user_name = "ahmed"
+print(user_name)
+print(user_name.upper())
+print(user_name.lower())
+print(user_name.islower())
+print(user_name.isupper())
+print(user_name.capitalize())
+print(user_name.title())
+
+
+
