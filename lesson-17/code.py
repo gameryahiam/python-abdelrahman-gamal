f82de18f1d5e96d07d150944ac52a299ac1a405d
gameryahiam <yahiamahmoud357@gmail.com>
2026-08-16 22:30:37 +0300

    add README and code files for lesson 17 and lesson 18

---
+++ lesson-17/code.py
@@ -0,0 +1,9 @@
+from math import *
+print(round(7.8))
+print(floor(7.8))
+print(ceil(7.8))
+print(pow(2, 3))
+print(2**3)
+print(abs(-7.8))
+print(max(7, 5))
+print(min(7, 5))
---
+++ lesson-18/code.py
@@ -0,0 +1,17 @@
+input("what is your name? ")
+name = input("what is your name? ")
+print("Hello " + name)
+
+age = input("what is your age? ")
+print("You are " + age + " years old.")
+
+
+
+
+
+
+
+
+
+
+
