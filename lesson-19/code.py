c303ca948341abb3e5a49afd50b0cab0bde672d5
gameryahiam <yahiamahmoud357@gmail.com>
2026-08-17 23:41:03 +0300

    add README and code files for lesson 19 and lesson 20

---
+++ lesson-19/code.py
@@ -0,0 +1,12 @@
+num1 = input("enter num:  ")
+num2 = input("enter other num:  ")
+result = float(num1) + float(num2) 
+result2 = int(num1) + int(num2) 
+
+
+
+
+print(result)
+
+
+print(result2)
---
+++ lesson-20/code.py
@@ -0,0 +1,16 @@
+x1 = input("enter lan:  ")
+x2 = input("enter other lan:  ")
+
+
+print('i love '+ x1)
+print('i love '+ x2)
+
+
+lan1 = 'i love '+ x1
+lan2 = 'i love '+ x2
+
+print(lan1)
+print(lan2)
+
+
+
