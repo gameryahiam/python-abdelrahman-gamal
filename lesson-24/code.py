1a6203b45f65c1cacb22d28397e25785f8da6acc
gameryahiam <yahiamahmoud357@gmail.com>
2026-08-23 00:45:06 +0300

    add README and code files for lesson 24

---
+++ lesson-24/code.py
@@ -0,0 +1,15 @@
+info = {
+"name": "Abdelrahman Gamal",
+"age": 20,
+"city": "Cairo"
+}
+
+
+print(info)
+print(info["name"])
+print(info["age"])
+print(info["city"])
+print(info.get("name"))
+print(info.get("age","Not Found"))  
+
+
