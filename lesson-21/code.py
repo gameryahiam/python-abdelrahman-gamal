5fbff647cb4d7df09497a17d2234ce83e4935584
gameryahiam <yahiamahmoud357@gmail.com>
2026-08-23 00:12:48 +0300

    add README and code files for lesson 21 and lesson 22

---
+++ lesson-21/code.py
@@ -0,0 +1,10 @@
+listas =["ahmed" , 12 , True , 3.14 , [1,2,3] , False]
+
+print(listas)  
+
+print(listas[0])
+
+print(listas[4][1])
+
+listas[1] = 'mahmoud'
+print(listas)
---
+++ lesson-22/code.py
@@ -0,0 +1,17 @@
+z =[1, 2, 3]
+x = [5, 10, 15]
+p =[4,64,468,498,49,6846,1,98,561,64,9749,149,47,479,1271244514]
+y =[21,12,2,0,8,9,99,57,565,45]
+x.append(20)
+x.insert(0, 0)
+x.extend(z)
+y.sort()
+p.sort(reverse=True)
+y.remove(565)
+ 
+
+
+print(x)
+print(z)
+print(y)
+print(p)
